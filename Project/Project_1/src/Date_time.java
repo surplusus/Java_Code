42fabef01ebfce45ec9cddab57f70606e757c397
AZIT_REMi_2018 <amorigeta+git@gmail.com>
2018-11-25 17:45:42 +0900

    밀린 것들

--- Project/Project_1/src/Date_time.java
+++ Project/Project_1/src/Date_time.java
@@ -1,7 +1,8 @@
-package examples;
+
 
 import java.io.IOException;
-import javax.servlet.ServletConfig;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,50 +10,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Override_dogetpost
+ * Servlet implementation class Date_time
  */
-@WebServlet("/Override_dogetpost")
-public class Override_dogetpost extends HttpServlet {
+@WebServlet("/today")
+public class Date_time extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Override_dogetpost() {
+    public Date_time() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-		System.out.println("init test ȣ��!");
-	}
-
-	/**
-	 * @see Servlet#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-		System.out.println("destroy");
-	}
-
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("<title>현재 시간</title>");
+		writer.println("<style>");
+		writer.println("body{font-size: 3rem;}");
+		writer.println("</style>");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<a href=\"index.html\">홈으로</a>");
+		writer.println("<br>현재 시간 : 2018/1/5 14:06");
+		
+		writer.println("</body>");
+		writer.println("</html>");
+		
+		writer.close();
 	}
 
 	/**
